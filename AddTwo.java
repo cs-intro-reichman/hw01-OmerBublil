da2d63889f542ba0645ec5f9661bd345442653f7
OmerBublil <omer.bublil@post.runi.ac.il>
2023-12-21 14:27:14 +0200

    Update AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -4,9 +4,7 @@
 public class AddTwo {
 	// start the main method
 	public static void main(String[] args) {
-		// Put your code here
-				// Put your code here	
-		 int a = Integer.parseInt (args[0]);
+		int a = Integer.parseInt (args[0]);
         int b = Integer.parseInt (args [1]);
         System.out.println(a + " + " + b + " = " + (a + b));	
 	}
